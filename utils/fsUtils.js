797368b40f6964978780580a816074a9b5dd54d7
dhuw <hazlewood.dyl@gmail.com>
2022-09-07 19:05:07 -0700

    util final

--- utils/fsUtils.js
+++ utils/fsUtils.js
@@ -1,2 +1,17 @@
 const fs = require('fs');
 const util = require('util');
+const readFromFile = util.promisify(fs.readFile);
+
+/**
+ * wrting data to json file and destination
+ *  @param {string} destination writing file
+ *  @param {object} content the content written
+ *  @returns {void} void
+ */
+
+const writeToFile = (destination, content) =>
+fs.writeFile(destination, JSON.stringify(content, null, 3), (err) =>
+   err ? console.error(err) : console.info(`\nData written to ${destination}`)
+ );
+
+ module.exports = { readFromFile, writeToFile};
